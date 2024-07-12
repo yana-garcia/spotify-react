92f05f1f171b813d74670d54dad8bd37625e6152
yana garcia <54054304+yana-garcia@users.noreply.github.com>
2024-07-13 02:27:14 +0800

    player, functions
    player, functions

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
 import {albumsData} from '../assets/assets'
+import {songsData} from '../assets/assets'
 
 const DisplayHome = () => {
   return (
@@ -11,6 +13,12 @@ const DisplayHome = () => {
             <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
             <div className='flex overflow-auto'>
             {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+            </div>
+        </div>
+        <div className='mb-4'>
+            <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
+            <div className='flex overflow-auto'>
+            {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
             </div>
         </div>
     </>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
+
+    const navigate = useNavigate()
+
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
         <div className='bg-[#121212] h-[25%] rouded flex flex-col justify-around'>
-            <div className='flex items-center gap-3 pl-8 pt-3.5 pb-2 cursor-pointer'>
+            <div onClick={() => navigate('/spotify-react')} className='flex items-center gap-3 pl-8 pt-3.5 pb-2 cursor-pointer'>
                 <img className='w-20' src={assets.spotify_logo_sidebar} alt="" />
             </div>
-            <div className='flex items-center gap-3 pl-8 pt-2 pb-2 cursor-pointer'>
+            <div onClick={() => navigate('/spotify-react')} className='flex items-center gap-3 pl-8 pt-2 pb-2 cursor-pointer'>
                 <img className='w-6' src={assets.home_icon} alt="" />
                 <p className='font-bold'>Home</p>
             </div>
